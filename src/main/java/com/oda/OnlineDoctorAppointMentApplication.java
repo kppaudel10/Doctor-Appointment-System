2153c28d8b3d10438814dd515798bcdfa5a3c2ef
Kul Paudel <kppaudel56@gmail.com>
2022-03-13 10:39:25 +0545

    project started

---
+++ src/main/java/com/oda/OnlineDoctorAppointMentApplication.java
@@ -0,0 +1,13 @@
+package com.oda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineDoctorAppointMentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineDoctorAppointMentApplication.class, args);
+    }
+
+}
