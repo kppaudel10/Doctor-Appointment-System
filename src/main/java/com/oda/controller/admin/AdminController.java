8854d6276ad01e262e1601fce71843427099cc0b
Kul Paudel <kppaudel56@gmail.com>
2022-04-30 18:49:57 +0545

    add all admin page

--- src/main/java/com/oda/controller/admin/AdminController.java
+++ src/main/java/com/oda/controller/admin/AdminController.java
@@ -1,6 +1,8 @@
 package com.oda.controller.admin;
 
+import com.oda.authorizeduser.AuthorizedUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,5 +18,25 @@ public class AdminController {
     @GetMapping("/home")
     public String getAdminHomePage(){
         return "admin/adminhomepage";
+    }
+
+    @GetMapping("/doctor-view")
+    public String getDoctorViewPage(){
+        return "admin/viewdoctor";
+    }
+
+    @GetMapping("/patient-view")
+    public String getPatientViewPage(){
+        return "admin/viewpatient";
+    }
+
+    @GetMapping("/doctor-request")
+    public String getDoctorRequestPage(){
+        return "admin/doctorrequest";
+    }
+
+    @GetMapping("/patient-request")
+    public String getPatientRequestPage(){
+        return "admin/patientrequest";
     }
 }
